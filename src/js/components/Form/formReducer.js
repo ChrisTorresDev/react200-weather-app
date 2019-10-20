ce3bc1fd6e014b4acd4e8ea8400f12961a2cb168
Chris Torres <hwct83@gmail.com>
2019-10-20 15:03:37 -0700

    added stateto formReducer file

--- src/js/components/Form/formReducer.js
+++ src/js/components/Form/formReducer.js
@@ -1,60 +1,17 @@
 const defaultState = {
+  city: '',
   temp: '',
   pressure: '',
   humidity: '',
   lowestTemp: '',
   highestTemp: '',
-  windSpeed: ''
+  windSpeed: '',
+  information: []
 };
 
-export default function cityInfoReducer(state =defaultState, action) {
-  const { type, payload } = action;
-
-  switch (type) {
-    case 'GET_TEMP': {
-      return {
-        ...state,
-        temp: payload.temp
-      };
-    }
-
-    case 'GET_PRESSURE': {
-      return {
-        ...state,
-        pressure: payload.pressure
-      };
-    }
-
-    case 'GET_HUMIDITY': {
-      return {
-        ...state,
-        humidity: payload.humidity
-      };
-    }
-
-    case 'GET_LOWEST_TEMP': {
-      return {
-        ...state,
-        lowestTemp: payload.lowestTemp
-      };
-    }
-
-    case 'GET_HIGHEST_TEMP': {
-      return {
-        ...state,
-        highestTemp: payload.highestTemp
-      };
-    }
-
-    case 'GET_WIND_SPEED': {
-      return {
-        ...state,
-        windSpeed: payload.windSpeed
-      };
-    }
-    
-    default: {
-      return state;
-    }
+const formReducer=(state=defaultState.action) => {
+  return {
+    ...state,
+    city: action.city
   }
 }
